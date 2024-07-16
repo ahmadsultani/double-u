7baac49260db37763906c8f8aa13d4b0a692d13f
ahmadsultani <ahmadsultanidayanullah@gmail.com>
2024-07-16 08:59:30 +0800

    feat: mianhae

---
+++ Web/mianhae/script.js
@@ -0,0 +1,30 @@
+const btn1 = document.getElementById("btn-1");
+const btn2 = document.getElementById("btn-2");
+
+btn1.addEventListener("click", () => {
+  Swal.fire({
+    title: "Yeyyy",
+    text: "Maaciyeyo",
+    icon: "love",
+  });
+});
+
+btn2.addEventListener(
+  "mouseover",
+  () => {
+    btn2.style.position = "absolute";
+    btn2.style.left = Math.random() * 100 + "%";
+    btn2.style.top = Math.random() * 100 + "%";
+  },
+  false
+);
+
+btn2.addEventListener(
+  "click",
+  () => {
+    btn2.style.position = "absolute";
+    btn2.style.left = Math.random() * 100 + "%";
+    btn2.style.top = Math.random() * 100 + "%";
+  },
+  false
+);
